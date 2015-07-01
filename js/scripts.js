afd9529174a73fdc9487e13879874fcd87b7986c
enrique esparza & paul morin <student@epicodus.com>
2015-07-01 16:44:33 -0700

    still a work in %$^%^&% progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,55 @@
-var countUpBy = function(number) {
-    return "25";
-};
+function Burger(cost) {
+  this.cost = 6;
+}
+
+// Burger.prototype.quantity = function(quantity) {
+//   this.cost *= quantity
+// }
+
+Burger.prototype.size = function(size) {
+  if(size === "Champ") {
+    this.cost += 4;
+  } else if(size === "House") {
+    this.cost += 2;
+  } else {
+    this.cost;
+  }
+}
+
+Burger.prototype.toppings = function(toppings) {
+  if(toppings === "Bacon and Cheese") {
+    this.cost += 3;
+  } else if(toppings === "Bacon"){
+    this.cost += 2;
+  } else if(toppings === "Cheese"){
+    this.cost += 1;
+  } else{
+    this.cost;
+  }
+}
 
 
-$(document).ready(function() {
-  $('countUpBy form').submit(function(event) {
-    var userNumbers = ["user_number", "multiplier"];
 
-    userNumbers.forEach(function(userNumber) {
-      var userInput = $("input#" + userNumber).val();
-      $("." + userNumber).text(userInput);
+
+  $(document).ready(function(){
+    var newBurger = new Burger();
+    newBurger.cost = 0;
+
+    $("form#order").submit(function(event){
+      newBurger.cost = 6;
+      // var inputtedQuantity = parseInt($("input#quantity").val());
+      var inputtedSize = $("input#size").val();
+      var inputtedToppings = $("input#toppings").val();
+
+      // newBurger.quantity(inputtedQuantity)
+      newBurger.size(inputtedSize)
+      newBurger.toppings(inputtedToppings)
+
+      $(".order-total").text(newBurger.cost);
+
+      event.preventDefault();
+
+
+
     });
-
-
-
-    $("#result").show();
-    event.preventDefault();
-  });
 });
-
-
-
-// $(document).ready(function() {
-//   $("form#leap-year").submit(function(event) {
-//     var year = parseInt($("input#year").val());
-//     var result = leapYear(year);
-//
-//     $(".year").text(year);
-//     if (!result) {
-//       $(".not").text("not");
-//     }
-//
-//     $("#result").show();
-//     event.preventDefault();
-//   });
-// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,37 @@
-describe('countUpBy', function(){
-  it("will count up to a number by multiples of a 2nd number", function(){
-    expect(countUpBy("25")).to.equal("25");
+describe("Burger", function()  {
+
+  describe("cost", function () {
+
+    it("will return a base price of 6 for our most basic burger burger", function () {
+      var testBurger = new Burger()
+      expect(testBurger.cost).to.equal(6)
+    });
   });
 
+  describe("quantity", function() {
+    it("will return a price based on quantity", function() {
+      var testBurger = new Burger()
+      testBurger.quantity(2)
+      expect(testBurger.cost).to.equal(12)
+
+    });
+  });
+
+  describe("size", function() {
+    it("will return a price based on size", function() {
+      var testBurger = new Burger()
+      testBurger.size("House")
+      expect(testBurger.cost).to.equal(8)
+    });
+  });
+
+  describe("toppings", function(){
+    it("will return a price based on toppings", function(){
+      var testBurger = new Burger()
+      testBurger.toppings("Bacon")
+      expect(testBurger.cost).to.equal(8)
+    });
+  });
 
 
 
